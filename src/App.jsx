0af8acc49803aa43169b025d54059d7104aa0d4e
Ahmad Ali <xevenahmad@gmail.com>
2024-12-07 17:21:05 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import './App.css';
+
+function App() {
+  return <h1 className='text-primary-peach-light_200 bg-primary-yellow-light_50 border-secondary-rosegold-light_200'>Hello world!</h1>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,125 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          //Color shades kit - #FF6060
+          // Primary Peach
+          peach: {
+            DEFAULT: '#FF6060', // Your main primary-peach color
+            light_50: '#660000', // Lighter shade
+            light_100: '#FFCCCC',
+            light_200: '#FF9999',
+            light_300: '#FF6666',
+            light_400: '#FF3333',
+            dark_600: '#CC0000',
+            dark_700: '#990000',
+            dark_800: '#660000',
+            dark_900: '#330000',
+            dark_950: '#1A0000', // Darker shade
+          },
+
+          //Color shades kit - #FBBE84
+          // Primary Yellow
+          yellow: {
+            DEFAULT: '#FBBE84', // Your main primary-yellow color
+            light_50: '#FFE469',
+            light_100: '#FDE5CE',
+            light_200: '#FCCA9C',
+            light_300: '#FAB06B',
+            light_400: '#F99639',
+            dark_600: '#C66306',
+            dark_700: '#944A05',
+            dark_800: '#633103',
+            dark_900: '#311902',
+            dark_950: '#1B0009', // Darker shade
+          },
+
+          //Color shades kit - #FF9696
+          //Primary Pink
+          pink: {
+            DEFAULT: '#FF9696', // Your main pink color
+            light_50: '#FFE5E5',
+            light_100: '#FFCCCC',
+            light_200: '#FF9999',
+            light_300: '#FF6666',
+            light_400: '#FF3333',
+            dark_600: '#CC0000',
+            dark_700: '#990000',
+            dark_800: '#660000',
+            dark_900: '#330000',
+            dark_950: '#1A0000', // Darker shade
+          },
+        },
+
+        secondary: {
+          //Color shades kit - Dark Gray #FF9696
+          darkgray: {
+            DEFAULT: '#595959', // Your main secondary color
+            light_50: '#F2F2F2', // Lighter shade
+            light_100: '#E6E6E6',
+            light_200: '#CCCCCC',
+            light_300: '#B3B3B3',
+            light_400: '#999999',
+            dark_600: '#CCCCCC',
+            dark_700: '#4D4D4D',
+            dark_800: '#333333',
+            dark_900: '#1A1A1A',
+            dark_950: '#0D0D0D', // Darker shade
+          },
+          //Color shades kit - Bright Gray #E0E4EA
+          brightgray: {
+            DEFAULT: '#E0E4EA', // Your main secondary color
+            light_50: '#F2F2F2',
+            light_100: '#E6E6E6',
+            light_200: '#CCCCCC',
+            light_300: '#B3B3B3',
+            light_400: '#999999',
+            dark_600: '#666666',
+            dark_700: '#4D4D4D',
+            dark_800: '#333333',
+            dark_900: '#1A1A1A',
+            dark_950: '#0D0D0D', // Darker shade
+          },
+          //Color shades kit - Light Yellow #FFF2CC
+          lightyellow: {
+            DEFAULT: '#FFF2CC',
+            light_50: '#FFF9E5',
+            light_100: '#FFF2CC',
+            light_200: '#FFE699',
+            light_300: '#FFD966',
+            light_400: '#FFCC33',
+            dark_600: '#CC9900',
+            dark_700: '#997300',
+            dark_800: '#664D00',
+            dark_900: '#332600',
+            dark_950: '#1A1300', // Darker shade
+          },
+          //Color shades kit - Royal Gold #EEC8C0
+          rosegold: {
+            DEFAULT: '#EEC8C0', // Your main rosegold color
+            light_50: '#EEC8C0',
+            light_100: '#EEC8C0',
+            light_200: '#EEC8C0',
+            light_300: '#DE9587',
+            light_400: '#D4725E',
+            dark_600: '#A13F2B',
+            dark_700: '#782F21',
+            dark_800: '#501F16',
+            dark_900: '#28100B',
+            dark_950: '#140805', // Darker shade
+          },
+          white: {
+            DEFAULT: '#FFFFFF', // Your main white color
+          },
+          black: {
+            DEFAULT: '#000000', // Your main black color
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
