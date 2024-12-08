60107a86d4cfad0530adabc82b6c34e0de26ed52
Ahmad Ali <xevenahmad@gmail.com>
2024-12-08 14:55:01 +0500

    signin page start

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,7 +1,9 @@
 import LoginBg from "./login-bg.png";
 import ProfileLogo from "./Ellipse 9.jpg";
+import Logo from "./logo.jpg";
 
 export const IMAGES = {
   LOGIN_BG: LoginBg,
   PROFILELOGO: ProfileLogo,
+  LOGO: Logo
 };
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,51 @@
 import { TextField } from '@mui/material';
+import { IMAGES } from '../assets/images';
 
 const SignIn = () => {
   return (
     <div>
-      <TextField id='outlined-basic' label='Outlined' variant='outlined' />
+      <div className='container mx-auto p-4'>
+        <div className='grid grid-cols-12 gap-4'>
+          <div className='lg:col-span-6 md:col-span-6 sm:col-span-12 col-span-12 flex flex-col justify-center items-center h-screen gap-4'>
+            <div>
+              <img src={IMAGES.LOGO} alt='' />
+            </div>
+            <TextField
+              id='first-field'
+              label='First Field'
+              className='rounded-full'
+              variant='outlined'
+              fullWidth
+            />
+             <TextField
+              id='first-field'
+              label='First Field'
+              variant='outlined'
+              fullWidth
+            />
+             <TextField
+              id='first-field'
+              label='First Field'
+              variant='outlined'
+              fullWidth
+            />
+             <TextField
+              id='first-field'
+              label='First Field'
+              variant='outlined'
+              fullWidth
+            />
+          </div>
+          <div className='lg:col-span-6 md:col-span-6 sm:col-span-12 col-span-12'>
+            <TextField
+              id='second-field'
+              label='Second Field'
+              variant='outlined'
+              fullWidth
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
